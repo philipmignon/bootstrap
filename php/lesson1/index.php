ec8ed7b962e8e026b59e9198d218ba3dd0179154
Trevor Greenleaf <info@trevorgreenleaf.com>
2014-09-16 06:25:30 -0700

    Moved folders around added php lesson 1 document

---
+++ php/lesson1/index.php
@@ -0,0 +1,139 @@
+<?php 
+
+//  output content to the browser
+echo "hello";
+
+// ====================================================================
+// VARIABLES
+// http://www.w3schools.com/php/php_variables.asp
+
+	// string
+	$cat = "Baba Do";
+
+	// integer
+	$cats = 10;
+
+
+// ====================================================================
+// FUNCTIONS - little reusable machines
+// http://www.w3schools.com/php/php_functions.asp
+
+	function make_cake(){
+		// echo out a string
+		echo "i made a cake";
+	}
+
+	function make_bread(){
+		// echo out a string
+		return "i made some bread";
+	}
+
+
+// next we need to call the make_cake function
+// we do this by calling 
+	make_cake();
+
+// or to call the return funciton we need to echo it out to see it
+	echo make_bread();
+
+
+// ====================================================================
+// ARRAYS - just a list of items
+// http://www.w3schools.com/php/php_arrays.asp
+
+	// an array of stings  
+	$animals = array("cats", "dogs", "rats", "bats", "hats?"); 
+
+	// an array of integers  
+	$numbers = array(5, 6, 8, 10, 9, 12); 
+
+	// getting single pieces of data from the array
+	// array pockets start with 0 so the first pocket will be 0.
+
+	// get pocket 1 from the animals array
+	echo $animals[1]; // identify which pocket from the array you want to grab
+
+	// get pocket 1 from the numbers array
+	echo $numbers[1];
+	echo $numbers[0];
+	echo $numbers[5];
+
+	// looping through the data
+	// count the number items
+	$array_length = count($animals);
+	// the loop through the array for the number of pockets in the array
+	for( $x=0; $x<$array_length; $x++){
+	  echo $animals[$x];
+	}
+		
+
+// ====================================================================
+	// ASSOCIATIVE ARRAYS - arrays that has a key value pair
+	// http://www.w3schools.com/php/php_arrays.asp
+	$age = array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+	// or
+	$age = array(
+			"Peter"=>"35",
+			"Ben"=>"37",
+			"Joe"=>"43"
+	);
+// getting single pieces of data from the array
+	echo $age["Peter"];
+	echo $age["Ben"];
+
+
+// looping through the data 
+// loop through the $age array and get the key and value pair
+foreach($age as $key=>$value){
+  	echo $key;
+  	echo $value;
+}
+
+// ====================================================================
+// Working with strings and vars with concatenation
+
+$cat = "Baba Do";
+echo "My cats name is ".$cat;
+
+// ====================================================================
+// Adding html tags
+
+$cat = "Frank";
+
+echo "<h1>".$cat."</h1><br>";
+echo "<p>".$cat." is such an awesome little cat.</p><br>";
+
+// ====================================================================
+// Working with vars and numbers
+
+$x = 1;
+$y = 10;
+
+$my_number = $x + $y;
+
+// display the number 11
+echo $my_number;
+
+
+// ====================================================================
+// More complex functions with vars
+	function make_taco($type){
+		echo "I made a ".$type." taco";
+	}
+
+	make_taco("corn");
+?>
+
+
+<!-- Wrapping HTML around you PHP tags -->
+<h1><?php echo $cat; ?></h1>
+
+
+
+
+
+
+
+
+
+
