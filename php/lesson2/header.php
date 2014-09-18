22606290224e0af4d70447ee402d10e637dfd8ab
Trevor Greenleaf <info@trevorgreenleaf.com>
2014-09-17 20:44:57 -0700

    Replaced the first function with the new better function

--- php/lesson2/header.php
+++ php/lesson2/header.php
@@ -1,3 +1,18 @@
+<?php
+function is_page($page){
+	// get current URL script part
+	$script   = $_SERVER['SCRIPT_NAME'];
+	// strip out location of the folder paths
+	$current_page = str_replace("/bootstrap/php/lesson2/","",$script);
+
+	// remove the .php from the path
+	$current_page = str_replace(".php","",$current_page);
+
+	if($current_page == $page){
+		return 'active';
+	}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -5,7 +20,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Includes and Fancy</title>
+    <title>
+	<?php 
+		if(is_page('index')){
+			echo 'home page!';
+		}
+		if(is_page('about')){
+			echo 'the about page!';
+		}
+		if(is_page('contact')){
+			echo 'the contact page!';
+		}
+	
+	?>
+
+    </title>
 
     <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<style>
@@ -17,32 +46,13 @@
 
 </head>
 <body>
-<?php
-
-// $script   = $_SERVER['SCRIPT_NAME'];
-// echo $script;
-
-function is_page($page){
-	// get current URL script part
-	$script   = $_SERVER['SCRIPT_NAME'];
-	// strip out location of the folder paths
-	$current_page = str_replace("/bootstrap/php/lesson2/","",$script);
-
-	// remove the .php from the path
-	$current_page = str_replace(".php","",$current_page);
-
-	if($current_page == $page){
-		echo 'active';
-	}
-}
-?>
 
 <div class="container">
 
 	<ul class="nav nav-pills">
-		<li class="<?php is_page('index'); ?>"><a href="index.php">Home</a></li>
-		<li class="<?php is_page('about'); ?>"><a href="about.php">About</a></li>
-		<li class="<?php is_page('contact'); ?>"><a href="contact.php">Contact</a></li>
+		<li class="<?php echo is_page('index'); ?>"><a href="index.php">Home</a></li>
+		<li class="<?php echo is_page('about'); ?>"><a href="about.php">About</a></li>
+		<li class="<?php echo is_page('contact'); ?>"><a href="contact.php">Contact</a></li>
 	</ul>
 
 <hr>
