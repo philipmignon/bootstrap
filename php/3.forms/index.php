2e954a361213563416e30b4b5f8e8fe878c56c00
Trevor Greenleaf <info@trevorgreenleaf.com>
2014-09-22 19:28:18 -0700

    Made updates to form basics file

--- php/3.forms/index.php
+++ php/3.forms/index.php
@@ -1,16 +1,17 @@
 <?php
 
-	// echo "<pre>";
-	// print_r($_POST);
-	// echo "</pre>";
+	echo "<pre>";
+		print_r($_POST);
+	echo "</pre>";
 
 	// process the form
 	// if form was submited
-	if(isset($_POST['cat_name'])){
+	if(isset($_POST['first_name'])){
 		// set var
-		$cat = $_POST['cat_name'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
 		// add var to alert and echo out 
-		echo '<div class="alert alert-success">'.$cat.'</div>';
+		echo '<div class="alert alert-success">'.$first_name.' '.$last_name.'</div>';
 	}
 ?>
 
@@ -20,21 +21,24 @@
 	<meta charset="UTF-8">
 	<title>Working with forms</title>
 	  <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	
 </head>
 <body>
 
-	<div class="col-xs-6 well">
-		<h2>Enter Cat Info</h2>
-		<form role="form" action="" method="POST">
-		  <div class="form-group">
-		    <label for="cat_name">Name</label>
-		    <input type="text" class="form-control" id="cat_name" name="cat_name">
-		  </div>
-		  <button type="submit" class="btn btn-primary">Submit</button>
-		</form>
-	</div>
 
-	
+
+<form role="form" action="" method="POST">
+
+    <label for="first_name">First Name</label>
+    <input type="text" id="first_name" name="first_name">
+
+    <label for="last_name">Last Name</label>
+    <input type="text" id="last_name" name="last_name">
+
+  <button type="submit" >Submit</button>
+</form>
+
+
 </body>
 </html>
 
