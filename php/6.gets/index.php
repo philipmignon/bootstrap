3b55fadbfbdcf06d29bca66ccf76e53236c930da
Trevor Greenleaf <info@trevorgreenleaf.com>
2014-09-24 20:01:57 -0700

    Added git

--- php/6.gets/index.php
+++ php/6.gets/index.php
@@ -1,10 +1,3 @@
-<?php
-
-// http://localhost/cal/?sub=deal
-// echo $_GET['sub'];
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,7 +11,36 @@
 <div class="col-xs-7">
 
 
-<h1></h1>
+<h1>What do I have in the URL
+
+<?php
+
+echo $_GET['search'];
+echo '<br>';
+echo $_GET['color'];
+
+?>
+
+</h1>
+
+
+<form class="form-horizontal" role="form" action="" method="GET">
+  <div class="form-group">
+    <label for="search" class="col-sm-2 control-label">search</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="num2" name="search" value="<?php if($_GET['search']){ echo $_GET['search']; }?>">
+    </div>
+  </div>
+
+    <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-primary">Search</button>
+    </div>
+  </div>
+
+</form>
+
+
 
 </div>
 
