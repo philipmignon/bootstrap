df14650d8e0b038808da09fbb41b649b44961ed1
Trevor Greenleaf <info@trevorgreenleaf.com>
2014-10-08 20:22:56 -0700

    Added collapse deal

--- wordpress/wp-content/themes/people/page-about.php
+++ wordpress/wp-content/themes/people/page-about.php
@@ -1,41 +1,13 @@
 <?php get_header();
 /*
-Template Name: About Page
+Template Name: FAQ Page
 */
 ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-      	<h1><?php the_title(); ?></h1>
-     	<?php the_content(); ?>
-
-<?php
-
-$tabs = get_field('tabs');
-$i = 1;
-$x = 1;
-if($tabs){
-	echo '<ul class="nav nav-tabs" role="tablist">';
-		foreach($tabs as $tab){	?>
-		<li class="<?php if($i == 1){echo 'active';} ?>">
-			<a href="#<?php echo 'tab_'.$i; ?>" role="tab" data-toggle="tab"><?php echo $tab['tab_name'];?></a>
-		</li>
-		<?php $i++; ?>
-	<?php }
-	echo '</ul>';
-
-
-	echo '<div class="tab-content">';
-		foreach($tabs as $tab){	?>
-	 		<div class="tab-pane <?php if($x == 1){echo 'active';} ?>" id="<?php echo 'tab_'.$x; ?>">
-	 		<?php echo $tab['tab_content'];?>
-	 		</div>
-		<?php $x++; ?>
-	<?php }
-	echo '</div>';
-}
-
-?>
+  	<h1><?php the_title(); ?></h1>
+ 	<?php the_content(); ?>
 
 
   <?php endwhile; else: ?>
---
+++ wordpress/wp-content/themes/people/page-faq.php
@@ -0,0 +1,72 @@
+<?php get_header();
+/*
+Template Name: FAQ Page
+*/
+?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+
+      	<h1><?php the_title(); ?></h1>
+
+
+    	<?php the_content(); ?>
+<?php 
+
+$faqs = get_field('faqs');
+$i = 1;
+
+if($faqs){
+
+
+echo '<div class="panel-group" id="accordion">';
+	foreach($faqs as $faq)
+	{ ?>
+	
+	<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#<?php echo'collapse_'.$i; ?>">
+         <?php echo $faq['title']; ?>
+        </a>
+      </h4>
+    </div>
+    
+    <div id="<?php echo'collapse_'.$i;?>" class="panel-collapse collapse 
+    <?php if($i == 1) { echo'in';} ?>">
+
+      <div class="panel-body">
+ 			<?php echo $faq['content']; ?>
+       </div>
+    </div>
+  </div>
+  <?php $i++; ?>
+
+
+	<?php }
+echo '</div>';
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php endwhile; else: ?>
+    <p>Sorry, no pages matched your criteria.</p>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
